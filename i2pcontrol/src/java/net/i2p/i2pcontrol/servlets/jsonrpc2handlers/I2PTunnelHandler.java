28283eb3f5c188a66585b3a2d7201f73fac3b9c0
jeff <ampernand@gmail.com>
2014-07-31 10:22:06 -0400

    work in progress do not use

--- i2pcontrol/src/java/net/i2p/i2pcontrol/servlets/jsonrpc2handlers/I2PTunnelHandler.java
+++ i2pcontrol/src/java/net/i2p/i2pcontrol/servlets/jsonrpc2handlers/I2PTunnelHandler.java
@@ -10,11 +10,6 @@ import net.i2p.i2pcontrol.router.RouterManager;
 import net.i2p.router.RouterContext;
 import net.i2p.util.Log;
 
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-
-
 /**
  * Created by jeff on 7/26/14.
  *
@@ -41,40 +36,53 @@ public class I2PTunnelHandler implements RequestHandler {
     static {
         try {
             _context = RouterManager.getRouterContext();
-        } catch (Exception thrown)  {
+        } catch (Exception thrown) {
             _log.error("Unable to initialize Router Context.", thrown);
         }
     }
 
     @Override
     public String[] handledRequests() {
-        return new String[] {
-               "tunnel_new",
-               "tunnel_del",
-               "tunnel_start",
-               "tunnel_stop",
-               "tunnel_status"
+        return new String[]{
+                "tunnel_new",
+                "tunnel_del",
+                "tunnel_start",
+                "tunnel_stop",
+                "tunnel_status"
         };
     }
 
     JSONRPC2Response _tunnel_new(JSONRPC2Request req) {
         return null;
     }
-
+    JSONRPC2Response _tunnel_del(JSONRPC2Request req) {
+        return null;
+    }
+    JSONRPC2Response _tunnel_start(JSONRPC2Request req) {
+        return null;
+    }
+    JSONRPC2Response _tunnel_stop(JSONRPC2Request req) {
+        return null;
+    }
+    JSONRPC2Response _tunnel_status(JSONRPC2Request req) {
+        return null;
+    }
     @Override
     public JSONRPC2Response process(JSONRPC2Request request, MessageContext requestCtx) {
-        if (request.getMethod().equals("i2ptunnel")) {
-            return process(request);
-        } else {
-            return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, request.getID());
-        }
-    }
-
-    private JSONRPC2Response process(JSONRPC2Request request) {
-        JSONRPC2Error err = JSONRPC2Helper.validateParams(null, request);
-        if ( err != null ) {
-            return new JSONRPC2Response(err, request.getID());
+        String method_name = request.getMethod();
+        switch(method_name) {
+            case "tunnel_new":
+                return _tunnel_new(request);
+            case "tunnel_del":
+                return _tunnel_del(request);
+            case "tunnel_start":
+                return _tunnel_start(request);
+            case "tunnel_stop":
+                return _tunnel_stop(request);
+            case "tunnel_status":
+                return _tunnel_status(request);
         }
         return null;
     }
+
 }
